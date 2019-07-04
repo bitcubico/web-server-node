661d429d59b688de7accbaf5699f01b8db161bfe
mcubico <mcubico33@gmail.com>
2019-07-04 14:40:59 -0500

    Agrego un server con express en vez de http, y al server viejo le agrego un comentario

---
+++ server-old-1.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const port = 3000;
+
+// Aquí se escuchan todas las peticiones que se envíen al server
+http.createServer((req, res) => {
+    // Indico que la respuesta estará dada en formato Json
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    let data = {
+        Nombres: 'Mauricio',
+        Apellidos: 'Montoya Medrano',
+        Email: 'mcubico33@gmail.com',
+    }
+
+    res.write(JSON.stringify(data));
+
+    // Linea usada para indicar que se puede levantar el servidor
+    res.end();
+}).listen(port);
+
+console.log(`Web Server escuchando el el puerto ${port}`);
--- server.js
+++ server.js
@@ -1,21 +1,31 @@
-const http = require('http');
-
+const express = require('express');
+const http = express();
 const port = 3000;
 
-http.createServer((req, res) => {
-    // Indico que la respuesta estará dada en formato Json
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+// Con express podemos especificar las url que vamos a aceptar
+// con http escuchabamos todo sin filtro
 
+// Esta es la url del home (localhost:port/)
+http.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Esta es la url del home (localhost:port/home)
+http.get('/home', (req, res) => {
+    res.send('Hello World from Home');
+});
+
+// Esta url me retorna un Json (localhost:port/get/json)
+http.get('/get/json', (req, res) => {
     let data = {
         Nombres: 'Mauricio',
         Apellidos: 'Montoya Medrano',
         Email: 'mcubico33@gmail.com',
     }
 
-    res.write(JSON.stringify(data));
+    res.send(data);
+});
 
-    // Linea usada para indicar que se puede levantar el servidor
-    res.end();
-}).listen(port);
-
-console.log(`Web Server escuchando el el puerto ${port}`);
+http.listen(port, () => {
+    console.log(`Web Server escuchando el el puerto ${port}`);
+});
